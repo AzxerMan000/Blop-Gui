fc944ff1cbd5be1b57335ab7ae9b2bf1794c826c
Azxerman <bangjoof12@gmail.com>
2025-07-23 06:20:04 +0700

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -6,34 +6,8 @@ function BlopGui.CreateLib(titleText, theme)
     local player = Players.LocalPlayer
     local playerGui = player:WaitForChild("PlayerGui")
 
-local Themes = {
-    Dark = {
-        BackgroundColor = Color3.fromRGB(20, 20, 20),
-        bColor = Color3.fromRGB(40, 40, 40),
-        TextColor = Color3.new(1, 1, 1)
-    },
-    Light = {
-        BackgroundColor = Color3.fromRGB(240, 240, 240),
-        bColor = Color3.fromRGB(220, 220, 220),
-        TextColor = Color3.new(0, 0, 0)
-    },
-    Ocean = {
-        BackgroundColor = Color3.fromRGB(0, 70, 140),
-        bColor = Color3.fromRGB(0, 120, 200),
-        TextColor = Color3.new(1, 1, 1)
-    },
-    Blood = {
-        BackgroundColor = Color3.fromRGB(70, 0, 0),
-        bColor = Color3.fromRGB(140, 0, 0),
-        TextColor = Color3.new(1, 1, 1)
-    },
-    Leaf = {
-        BackgroundColor = Color3.fromRGB(34, 80, 34),
-        bColor = Color3.fromRGB(50, 120, 50),
-        TextColor = Color3.new(1, 1, 1)
-    }
-    }
 
+    local themes = loadstring(game:HttpGet(""))()
     local selectedTheme = themes[themeName] or themes["Dark"]
     
 
